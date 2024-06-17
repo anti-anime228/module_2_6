f99a744d2d92ddec0069e1a8add96efd990f2916
anti-anime228 <qwerty9228@bk.ru>
2024-06-17 16:49:03 +0300

    Add files via upload

--- module_2_6.py
+++ module_2_6.py
@@ -1,30 +1,14 @@
-# root_word = input("Введите число-ключ: ").lower()
-# other_words_input = input("Введите остальные слова через запятую ',': ").lower()
-# other_words = other_words_input.split(",")  # Разбиваем строку по ,
-
-#рабочая
-# def single_root_words(root_word, *other_words):
-#     same_words = []
-#     for o_word in other_words:
-#         if root_word in o_word:
-#             same_words.append(o_word)
-#     return same_words
-# print(single_root_words(root_word, other_words))
-
-
-# def single_root_words(root_word, *other_words):
-#     same_words = []
-#     for o_word in other_words:
-#         if root_word in o_word:
-#             same_words.append(o_word)
-#     return same_words
-
-phrase = ('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
-word = 'rich'
-def single_root_words():
+def single_root_words(root_word, *other_words):
     same_words = []
-    for i in phrase:
-        if word in phrase:
-            same_words.append(phrase)
-    print(same_words)
-single_root_words()
+    root_word = root_word.lower()
+    for o_word in other_words:
+        o_word = o_word.lower()
+        if root_word in o_word or o_word in root_word:
+            same_words.append(o_word)
+    return same_words
+
+
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+print(result1)
+print(result2)
